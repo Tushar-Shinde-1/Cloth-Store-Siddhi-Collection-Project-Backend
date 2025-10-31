4438c98cb7251c80d42d5fd0994025b83c7d9327
Your Name <your_email@example.com>
2025-10-31 21:22:35 +0530

    admin change

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Siderbar from './components/Siderbar'
 import { Routes, Route } from 'react-router-dom'
 import Add from './pages/Add'
